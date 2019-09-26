c4fc479db08c1d6946d679cf9f9ecc2f734ae5c3
mina alfy <5628914+minaalfy@users.noreply.github.com>
2019-09-26 12:43:01 +0400

    Add Cairo boundaries and center

--- constants.js
+++ constants.js
@@ -3,3 +3,5 @@ export const ABUDHABI_CENTER = {lat: 24.463581 , lng: 54.3255993}
 export const ABUDHABI_BOUNDS = { north: 26.0765, south: 22.3315138, west: 51.0, east: 56.4395001};
 export const BERLIN_CENTER = {lat: 52.52000659999999 , lng: 13.404954}
 export const BERLIN_BOUNDS = { north: 52.6754542, south: 52.338234, west: 13.088346, east: 13.7611175};
+export const CAIRO_CENTER = {lat: 30.0444196 , lng: 31.2357116}
+export const CAIRO_BOUNDS = { north: 30.1106024, south: 30.0083745, west: 31.2149558, east: 31.9019729};
